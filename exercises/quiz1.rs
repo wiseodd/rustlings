d4cdbe8c172febea21b057285b9b54943faae5d7
Agustinus Kristiadi <agustinus@kristia.de>
2024-04-29 12:19:06 -0400

    More solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    let price: i32 = if n_apples > 40 {
+        n_apples
+    } else {
+        n_apples * 2
+    };
+
+    return price;
+}
 
 // Don't modify this function!
 #[test]
